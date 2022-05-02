500733dabe6bb6d1267c42d22e51aaacd2b9a9fd
ga4ik <ga4ik@mail.ru>
2022-05-02 18:14:00 +0300

    realised simple alghorithm

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -23,12 +23,144 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Картинка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Карта";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(20, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(543, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Сделать заебись";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1074, 50);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(373, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(417, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(468, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(461, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Тыкнуть на исходную картинку - сменить местоположение источника освещения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1693, 583);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private OpenFileDialog openFileDialog1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button button3;
+        private PictureBox pictureBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,7 +1,105 @@
 namespace WinFormsApp1 {
     public partial class Form1 : Form {
+        
         public Form1() {
             InitializeComponent();
+        }
+        int x, y;
+        private void button1_Click(object sender, EventArgs e) {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                Image img =Image.FromFile(openFileDialog1.FileName);
+                if (img.Size != pictureBox1.Image.Size) {
+                    MessageBox.Show(" артинка и карта должны быть идентичных размеров");
+                }
+                else pictureBox2.Image = img;
+                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+
+            do_nice(250, 250);
+        }
+        void do_nice(int x,int y) {
+            const int range = 2;
+            const int sensivity = 2;
+            int[] light_pos = { x, y };
+            if (pictureBox1.Image != null &&
+                pictureBox2.Image != null) {
+                Bitmap b1 = new Bitmap(pictureBox1.Image);
+                Bitmap b2 = new Bitmap(pictureBox2.Image);
+                Bitmap result = new Bitmap(b1.Width, b1.Height);
+                for (int i = 1; i < b1.Width - 1; i++) {
+                    for (int j = 1; j < b1.Height - 1; j++) {
+                        int[] light_vect = { light_pos[0] - i, light_pos[1] - j };
+                        double[] norm_light_vect = normalize(light_vect);
+                        int x_grad = 0;
+                        int y_grad = 0;
+                        if (i - range > 0 && j - range > 0 && i + range < b1.Width && j + range < b1.Height) {
+                            x_grad = (int)Math.Floor((double)((b2.GetPixel(i - range, j).R - b2.GetPixel(i + range, j).R)/sensivity))*sensivity;
+                            y_grad = (int)Math.Floor((double)((b2.GetPixel(i, j - range).R - b2.GetPixel(i, j + range).R) / sensivity))* sensivity;
+                        }
+                        int[] bump_vector = { x_grad, y_grad };
+                        double[] norm_bump_vect = normalize(bump_vector);
+                        result.SetPixel(i, j, calc_bump(norm_light_vect, norm_bump_vect, b1.GetPixel(i, j)));
+                    }
+                }
+                pictureBox3.Image = result;
+            }
+        }
+        Color calc_bump(double[] light,double[] bump,Color p1) {
+            const int sensivity = 2;
+            double intensity = dot_product(light, bump)/2;
+            int R = (int)Math.Round(p1.R + Math.Floor((double)(intensity * 128/sensivity))*sensivity);
+            if (R > 255) R = 255;
+            if(R<0) R = 0;
+            int G = (int)Math.Round(p1.G + Math.Floor((double)(intensity * 128 / sensivity)) * sensivity);
+            if (G > 255) G = 255;
+            if (G < 0) G = 0;
+            int B = (int)Math.Round(p1.B + Math.Floor((double)(intensity * 128 / sensivity)) * sensivity);
+            if (B > 255)B = 255;
+            if (B < 0) B= 0;
+            //return Color.FromArgb((int)Math.Round(p1.R * intensity), (int)Math.Round(p1.G * intensity), (int)Math.Round(p1.B * intensity)); 
+            return Color.FromArgb(R,G,B);
+
+
+        }
+        double[] normalize(int[] a) {
+            double[] result = new double[a.Length];
+            double length = Math.Sqrt(a.Sum((cmp) => { return Math.Pow(cmp, 2); }));
+            for (int i = 0; i < a.Length; i++)
+                if (length != 0)
+                    result[i] = a[i] / length;
+                else result[i] = 0;
+            return result;
+        }
+        double dot_product(double[] x, double[] y) {
+            double result = 0;
+            for (int i = 0; i < x.Length; i++) { 
+                result+=x[i]*y[i];
+            }
+            return result;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) {
+            do_nice(x,y);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) {
+            x = e.X;
+            y = e.Y;
+            label1.Text = x.ToString();
+            label2.Text = y.ToString();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e) {
+           
         }
     }
 }
